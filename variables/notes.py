25e963588e357c2c7668229750b907b82397d89f
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-01-28 09:52:05 -0700

    Made notes and a cool program that asks your name

--- variables/notes.py
+++ variables/notes.py
@@ -1,1 +1,12 @@
 # Cairo Taylor, Variables Notes
+
+name = "Cairo"
+
+print("I'm Mr King Dice")
+print("I'm the gamest in the land")
+print(name, ", beware...")
+
+name = "Ooga Booga Man"
+print(name, ", beware", name)
+name = input("What is your name, traveler?:  ")
+print("Let me tell you the Story of the Ancients... and then you must go on your quest,", name)
