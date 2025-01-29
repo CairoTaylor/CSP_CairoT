2e053e469eb6d216b9ccdf49d2983b63052a29a3
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-01-29 09:52:58 -0700

    I worked on my practice

--- variables/notes.py
+++ variables/notes.py
@@ -10,3 +10,4 @@ name = "Ooga Booga Man"
 print(name, ", beware", name)
 name = input("What is your name, traveler?:  ")
 print("Let me tell you the Story of the Ancients... and then you must go on your quest,", name)
+print("The Story of the Ancients is a tale that can decide your future. Long ago, when the world was still all in one place, there was a king. and evil king. His name will surprise you... for it was", name, "just like yours.")
