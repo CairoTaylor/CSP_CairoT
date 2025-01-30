499612603c545bfbcd42418937c776fbb540b6e4
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-01-30 09:53:23 -0700

    I learned new code

--- template.c
+++ template.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+char name[] = "Cairo";
+int num = 14;
+float pi = 3.141592;
+
 int main(void){
-    printf("Hello world");
+    printf("Hello %s\n", name);
+    printf("%d\n", num);
+    printf("%f\n", pi);
+    printf("How are you doing today, %s? \n", name);
+    printf("Hello, my name is %s, I am %d years old, and my favorite number is %f. \n", name, num, pi)
     return 0;
 }
