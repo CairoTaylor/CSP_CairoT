07ac1eff840e98e42ba5dbdcaa0bb3e7ae71e7e7
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-01-31 09:50:36 -0700

    I made a goofy program

---
+++ pythong.py
@@ -0,0 +1,11 @@
+# Cairo Taylor, First Program
+name = input("What is your name, Traveler?:  ")
+print("Hello", name)
+print("My name is Biddle Diddle the Great")
+place = input("Where do you wish to see?:  ")
+print("I have been to", place,)
+reason = input("What geographical feature makes you want to go?:  ")
+print("The road there is difficult, and don't even get me started on how lame", reason, "is")
+print("I would not reccomend going,", name)
+print("The trip there will cost twice as much as you could ever own")
+print("*Biddle Diddle dabs and walks away*")
