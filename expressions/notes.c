25a0ec3daf93d10f9ea41b581e8998c2d61234fc
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-04 09:53:43 -0700

    I worked on my practice and finished notes

--- expressions/notes.c
+++ expressions/notes.c
@@ -1,10 +1,16 @@
 //Cairo Taylor, Expressions notes for C
 #include <stdio.h>
+#include <math.h> //this is what lets us use exponents
 //Integers use Int to set variable and %d to print
 //Floats use Float to set variable and %f to print
 //Strings use Char to set variable and %s to print
 int mynum;
 float percent;
+int add = 4+6;
+int mul = 4*6;
+float div = 6/4;
+int mod = 6/4;
+int ex = pow(5, 2);
 
 int main(void){
    printf("Type a number: \n");
@@ -12,5 +18,12 @@ int main(void){
    printf("Your number is %d \n", mynum);
    printf("Give me a percent as a decimal: \n");
    scanf("Your percent is %f", percent);
+   printf("%d\n", add);
+   printf("%d\n", mul);
+   printf("%.2f\n", div);
+   mul = 7*4;
+   printf("%d\n", mod);
+   printf("%d\n", ex);
+   printf("%d\n", mul);
     return 0;
 }
