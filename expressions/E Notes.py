6b3b6c83ccca6cd953f131aeb39295d0624501d7
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-05 09:52:42 -0700

    I worked on my C expressions practice

--- expressions/E Notes.py
+++ expressions/E Notes.py
@@ -3,16 +3,16 @@
 #floats are numbers with decimal points
 #algorithms are a set of intructions used to complete a task
 #division will always float
-#print(10+5)
-#print(10+5.5)
-#print(10-5)
-#print(10*5)
-#print(10/5)
-#print(10**5) #exponents
-#print(10.5//5) #gives whole number
-#print(10.5%5) #modulo/modulus/mod gives remainder
-#print(3+7*2)
-#print((10*17)**2)
+print(10+5)
+print(10+5.5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10**5) #exponents
+print(10.5//5) #gives whole number
+print(10.5%5) #modulo/modulus/mod gives remainder
+print(3+7*2)
+print((10*17)**2)
 
 pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482
 
---
+++ expressions/expressions.c
@@ -0,0 +1,10 @@
+//Cairo Taylor, Expressions Practice
+#include <stdio.h>
+#include <math.h>
+
+int ordooper1 = 24/8*4;
+
+int main(void){
+   printf("7-%d+6\n", ordooper1);
+    return 0;
+}
