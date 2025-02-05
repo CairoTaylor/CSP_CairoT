64091b83825279f071061ec62769f30b41b328b8
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-05 09:20:37 -0700

    I finished my python expressions practice

---
+++ expressions_practice.py
@@ -0,0 +1,20 @@
+#Cairo Taylor, Expressions Practice
+print(7-24/8*4+6)
+print(18/3-7+2*5)
+print(6*4/12+72/8-9)
+a = 17-6/2
+print(a+4*3)
+b = 1*4-2/2
+c = 6+2-3
+print(-2*b+c)
+d = 3-4*7
+e = d/5
+print(-1*e-2*24/6)
+f = 3*5**2/15
+g = 5-2**2
+print(f-g)
+h = 1**4*2**2+3**3
+print(h-2**5/4)
+i = 22/2-2*5
+j = 4-6/6
+print(i**2+j**2)
