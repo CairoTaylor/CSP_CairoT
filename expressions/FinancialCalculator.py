71963692b0b58c1357bb95104b528c2b5b71ac0c
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-06 09:48:34 -0700

    I worked on my financial calculator

--- expressions/FinancialCalculator.py
+++ expressions/FinancialCalculator.py
@@ -1,1 +1,29 @@
-#Cairo Taylor, Financial Calculator
+# Cairo Taylor, Financial Calculator
+
+# Write a print statement telling the user what the program is
+print("Hello! This is a program for calculating your monthly finances.")
+# Ask the user for monthly income
+income = float(input("What is your monthly income?\n"))
+# Ask for each expense (rent, utilities, groceries, transportation)
+rent = float(input("What is your monthly rent?\n"))
+utilities = float(input("What is your monthly cost of utilities?\n"))
+groceries = float(input("What is your monthly cost of groceries?\n"))
+transportation = float(input("What is your monthly cost of transportation?\n"))
+# Calculate savings as 10% of income
+
+# Calculate spending money: income - rent, utilities, groceries, transportation, and savings
+
+# Calculate % of rent (rent/income)
+
+# Calculate % of utilities (rent/utilities)
+
+# Calculate % of groceries (rent/groceries)
+
+# Calculate % of transportation (rent/transportation)
+
+# Tell the user category spending amount and percent for all categories
+
+# Tell the user how much spending money is available
+
+# Tell the user how much money is for saving
+
