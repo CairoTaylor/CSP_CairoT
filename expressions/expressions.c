99fc5668e3e234338bfc5ea801b7515013baa85a
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-07 03:45:27 +0000

    I finished my expressions practice

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -2,9 +2,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int ordooper1 = 24/8*4;
+int prb1;
+int prb2;
+int prb3;
+int prb4;
+int prb5;
+int prb6;
+int prb7;
+int prb8;
+int prb9;
 
 int main(void){
-   printf("7-%d+6\n", ordooper1);
-    return 0;
+prb1 = 7 - 24 / 8 * 4 + 6;
+printf("%d\n", prb1);
+prb2 = 18 / 3 - 7 + 2 * 5;
+printf("%d\n", prb2);
+prb3 = 6 * 4 / 12 + 72 / 8 - 9;
+printf("%d\n", prb3);
+prb4 = (17 - 6 / 2) + 4 * 3;
+printf("%d\n", prb4);
+prb5 = -2 * (1 * 4 - 2 / 2) + (6 + 2 - 3);
+printf("%d\n", prb5);
+prb6 = -1 * ((3 - 4 * 7) / 5) - 2 * 24 / 6;
+printf("%d\n", prb6);
+prb7 = (3 * pow(5, 2) / 15) - (5 - pow(2, 2));
+printf("%d\n", prb7);
+prb8 = (pow(1, 4) * pow(2, 2) + pow(3, 3)) - (pow(2, 5) / 4);
+printf("%d\n", prb8);
+prb9 = (pow(22 / 2 - 2 * 5, 2) + pow(4 - 6 / 6, 2));
+printf("%d\n", prb9);
+return 0;
 }
