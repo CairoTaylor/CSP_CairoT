a177b5cdf208daf6ce223f69953c2bab6259d310
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-07 09:52:42 -0700

    I made my financial calculator better

--- expressions/FinancialCalculator.py
+++ expressions/FinancialCalculator.py
@@ -15,19 +15,20 @@ savings = float(income / 10)
 # Calculate spending money: income - rent, utilities, groceries, transportation, and savings
 spending = float(income - rent - utilities - groceries - transportation)
 # Calculate % of rent (rent/income)*100
-perent = float(income / rent * 100)
+perent = float((rent / income) * 100)
 # Calculate % of utilities (rent/utilities)*100
-perutilities = float(income / utilities * 100)
+perutilities = float((utilities / income) * 100)
 # Calculate % of groceries (rent/groceries)*100
-pergroceries = float(income / groceries * 100)
+pergroceries = float((groceries / income) * 100)
 # Calculate % of transportation (rent/transportation)*100
-pertransportation = float(income / transportation * 100)
+pertransportation = float((transportation / income) * 100)
+perspending = float((spending / income) * 100)
 # Tell the user category spending amount and percent for all categories
-print("The percent of money from your income used for rent is", round(perent, 2))
-print("The percent of money from your income used for utilities is", round(perutilities, 2))
-print("The percent of money from your income used for groceries is", round(pergroceries, 2))
-print("The percent of money from your income used for transportation is", round(pertransportation, 2))
+print("The amount spent on rent is", round(rent, 2), "and that is", round(perent, 2), "percent of your income")
+print("The amount spent on rent is", round(utilities, 2), "and that is", round(perutilities, 2), "percent of your income")
+print("The amount spent on rent is", round(groceries, 2), "and that is", round(pergroceries, 2), "percent of your income")
+print("The amount spent on rent is", round(transportation, 2), "and that is", round(pertransportation, 2), "percent of your income")
 # Tell the user how much spending money is available
-print("The money you have left for spending is", round(spending, 2))
+print("The money you have left for spending is", round(spending, 2), "which is", round(perspending, 2), " percent of your income")
 # Tell the user how much money is for saving
-print("The amount of money you have saved for future use is", round(savings, 2))
+print("The amount of money you have saved for future use is", round(savings, 2), "which is 10 percent of your income")
--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -1,8 +1,15 @@
 //Cairo Taylor, Financial Calculator
 #include <stdio.h>
 
+int income;
+int rent;
+int utilities;
+int groceries;
+int transportation;
+int savings;
+int spending;
 
 int main(void){
-   printf("Hello World");
+   printf("Hello! This is a program for calculating your monthly finances, but now in a new flavor(C).");
     return 0;
 }
