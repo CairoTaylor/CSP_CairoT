5538d9bf2a619a2c36ec59869df23135ab12c1d3
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-07 09:26:46 -0700

    I finished my python financial calculator

--- expressions/FinancialCalculator.py
+++ expressions/FinancialCalculator.py
@@ -1,5 +1,6 @@
 # Cairo Taylor, Financial Calculator
 
+# Going top to bottom, the averages for all these things I found is 4897, 1828, 345, 606, and 1098
 # Write a print statement telling the user what the program is
 print("Hello! This is a program for calculating your monthly finances.")
 # Ask the user for monthly income
@@ -10,20 +11,23 @@ utilities = float(input("What is your monthly cost of utilities?\n"))
 groceries = float(input("What is your monthly cost of groceries?\n"))
 transportation = float(input("What is your monthly cost of transportation?\n"))
 # Calculate savings as 10% of income
-
+savings = float(income / 10)
 # Calculate spending money: income - rent, utilities, groceries, transportation, and savings
-
-# Calculate % of rent (rent/income)
-
-# Calculate % of utilities (rent/utilities)
-
-# Calculate % of groceries (rent/groceries)
-
-# Calculate % of transportation (rent/transportation)
-
+spending = float(income - rent - utilities - groceries - transportation)
+# Calculate % of rent (rent/income)*100
+perent = float(income / rent * 100)
+# Calculate % of utilities (rent/utilities)*100
+perutilities = float(income / utilities * 100)
+# Calculate % of groceries (rent/groceries)*100
+pergroceries = float(income / groceries * 100)
+# Calculate % of transportation (rent/transportation)*100
+pertransportation = float(income / transportation * 100)
 # Tell the user category spending amount and percent for all categories
-
+print("The percent of money from your income used for rent is", round(perent, 2))
+print("The percent of money from your income used for utilities is", round(perutilities, 2))
+print("The percent of money from your income used for groceries is", round(pergroceries, 2))
+print("The percent of money from your income used for transportation is", round(pertransportation, 2))
 # Tell the user how much spending money is available
-
+print("The money you have left for spending is", round(spending, 2))
 # Tell the user how much money is for saving
-
+print("The amount of money you have saved for future use is", round(savings, 2))
