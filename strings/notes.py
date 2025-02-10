9f1e09368d8b97dc79f87e1018ce8efef6fc614d
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-10 09:53:11 -0700

    I tool notes for strings

---
+++ strings/notes.py
@@ -0,0 +1,13 @@
+# Cairo Taylor, Strings Notes
+
+# A string is words in programming (data type) "" ''
+
+print("I'm Mr. King Dice")
+print('Im Mr. King Dice') # Use quotation marks when you need apostrophes
+
+name = input("What is your first name?\n").strip().capitalize() # Make sure to add () at the end
+print (f"You have a bad name, {name}.")
+
+num = input("Give me a number.\n")
+
+challenge = input("Do you wish to challenge me?\n")
