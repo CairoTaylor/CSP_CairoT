389c7d5573df49baad17157cad8b99d1110845b7
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-13 09:52:42 -0700

    I worked on a weird sentence generator

---
+++ strings/silly_sentences.c
@@ -0,0 +1,27 @@
+//Cairo Taylor, Silly Sentences
+#include <stdio.h>
+// String variables for user inputs (minimum of 3)
+
+char name[20];
+char place[20];
+char verbed[20];
+int number1[20];
+char cakeingredient[20];
+
+int main(void){
+printf("Hello. I am glad you are using my program. You see, you have invoked my power. My power is creating goofy and weird sentences.\nSo, when I ask you to fill in a word, please do so.\n");
+printf("What is a male name?\n");
+scanf("%s", name);
+printf("What is the name of the place your character lives in?\n");
+scanf("%s", place);
+printf("What is a past-tense verb?\n");
+scanf("%s", verbed);
+printf("What is your favorite number?\n");
+scanf("%d", number1);
+printf("What is an ingredient in cake?\n");
+scanf("%s", cakeingredient);
+printf("One day, %s was walking down the street in %s, when suddenly, ")
+// Insert variables into a sentence to show the user what monstrosity they have created (only 1 statement)
+
+    return 0;
+}
