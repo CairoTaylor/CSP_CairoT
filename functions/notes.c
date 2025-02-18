1781e93aae51744feca0352882985fcaec68086d
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-18 09:53:05 -0700

    I worked on notes

---
+++ functions/notes.c
@@ -0,0 +1,23 @@
+//cairo Taylor, Functions Notes
+#include <stdio.h>
+
+void add(int num1, int num2){
+    printf("%d\n", num1+num2);
+}
+
+char input(char type[20]){
+    char answer[50];
+    printf("Give me a %s:\n", type);
+    scanf("%s", answer);
+    return answer;
+}
+
+int main(void){
+   //printf("Hello World\n");
+   //add(12, 9);
+   input("name");
+   input("verb");
+   input("place");
+   printf("%s was %s until they somehow reached %s", input("name"), input("verb"), input("place"));
+    return 0;
+}
---
+++ functions/notes.py
@@ -0,0 +1,19 @@
+#Cairo Taylor, Functions Notes
+
+#Function is action stored by keyword
+
+#def add(num1, num2):
+#    print(num1+num2)
+
+#add(12, 20)
+#add(14, 5)
+#add(int(input("Tell me a number:\n")), 45)
+
+def user(word):
+    answer = input(f"Tell me a {word}:\n")
+    return answer
+
+name = user("name").capitalize()
+verbing = user("present tense verb").lower()
+place = user("place").capitalize()
+print(f"{name} was {verbing} to {place}.")
