31bf0c4eb895eb01898af9912f0558c3b5134634
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-19 09:41:46 -0700

    I remade my Hello World project but used functions instead

---
+++ functions/hello world updated.py
@@ -0,0 +1,22 @@
+#Cairo Taylor, Hello World Updated
+
+
+
+def title(count):
+    answor = input(f"Tell me person #{count}'s name:\n")
+    return answor
+
+first = title("1").capitalize()
+print(f"Hello, {first}! It's nice to meet you!")
+
+second = title("2").capitalize()
+print(f"Hello, {second}! It's nice to meet both you and {first}!")
+
+third = title("3").capitalize()
+print(f"Hello, {third}! It's nice to meet you, {second}, and {first}!")
+
+fourth = title("4").capitalize()
+print(f"Hello, {fourth}! It's nice to meet you, {third}, {second}, and {first}!")
+
+fifth = title("5").capitalize()
+print(f"Hello, {fifth}! It's nice to meet you, {fourth},  {third}, {second}, and {first}!")
--- functions/notes.py
+++ functions/notes.py
@@ -2,8 +2,8 @@
 
 #Function is action stored by keyword
 
-#def add(num1, num2):
-#    print(num1+num2)
+def add(num1, num2):
+    print(num1+num2)
 
 #add(12, 20)
 #add(14, 5)
