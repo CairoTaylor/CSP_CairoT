a1176024468097638e4524f6a59107526591cbef
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-21 09:49:04 -0700

    I FINALLY finished my updated hello world for C

--- functions/hellowrold.c
+++ functions/hellowrold.c
@@ -1,18 +1,31 @@
 //Cairo Taylor, Hello World Updated
 #include <stdio.h>
+#include <string.h>
 
+char asnwer[20];
 char first[20];
 char second[20];
-char three[20];
+char third[20];
 char fourth[20];
 char fifth[20];
 
-char inputer(char count){
+char* hellow(int count){
 printf("Tell me person #%d's name:\n", count);
-return ;
+scanf("%s", asnwer);
+return asnwer;
 }
 
 int main(void){
-printf("It's nice to meet you, %d!", input("one", 50));
+strcpy(first, hellow(1));  
+printf("It's nice to meet you, %s!\n", first);
+strcpy(second, hellow(2)); 
+printf("It's nice to meet you, %s, as well as %s!\n", second, first); 
+strcpy(third, hellow(3));  
+printf("It's nice to meet you, %s, as well as %s and %s!\n", third, second, first);
+strcpy(fourth, hellow(4));  
+printf("It's nice to meet you, %s, as well as %s, %s, and %s!\n", fourth, third, second, first);
+strcpy(fifth, hellow(5));  
+printf("It's nice to meet you, %s, as well as %s, %s, %s, and %s!\n", fifth, fourth, third, second, first);
     return 0;
 }
+// Remember the ART acronym: Always Remember Terhg
