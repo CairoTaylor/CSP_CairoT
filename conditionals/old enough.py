2786d186e5f1af24c74b5162bc144cdee1bdb8b8
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-26 09:33:42 -0700

    I finished my age checking thing

--- conditionals/old enough.py
+++ conditionals/old enough.py
@@ -8,7 +8,11 @@ elif age >= 6 and age < 15:
     print("You are old enough to go to school, however your parents have to take you.")
 elif age >= 15 and age < 16:
     print("You are old enough to go to school and get your learners permit, but no driving on your own yet.")
-elif age <= 16 and age > 18:
+elif age >= 100 and age <120:
+    print("Wow you are very old. How exactly are you using this program?")
+elif age >= 120:
+    print("How are you still alive??")
+elif age >= 16 and age < 18:
     print("You are old enough to go to school and get your drivers license, but no voting for you yet.")
 else:
     print("Congratulations! You can drive and vote, and you're also out of school!")
--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -1,13 +1,27 @@
 // Cairo Taylor, Old Enough Project
 #include <stdio.h>
 
-int age[20];
+int age;
 
 int main(void){
-printf("How old are you chumster?");
-scanf("%s", age);
+printf("How old are you chumster?\n");
+scanf("%d", &age);
 
-if(num < 6){}
+if(age < 6){
+    printf("Sorry, but you aren't even old enough to go to school.\n");
+}else if(age >= 6 && age <15){
+    printf("You are old enough to go to school, however your parents have to take you.\n");
+}else if(age >= 15 && age < 16){
+    printf("You are old enough to go to school and get your learners permit, but no driving on your own yet.\n");
+}else if(age >= 100 && age < 120){
+    printf("Wow you are very old. How exactly are you using this program?");
+}else if(age >= 16 && age < 18){
+    printf("You are old enough to go to school and get your drivers license, but no voting for you yet.\n");
+}else if(age >= 120){
+    printf("How are you still alive??");
+}else{
+    printf("Congratulations! You can drive and vote, and you're also out of school!\n");
+}
 
     return 0;
 }
