348914f1e5c26426475f02465634d90a88a31e9f
CairoTaylor <cairo.taylor@ucas-edu.net>
2025-02-26 09:11:12 -0700

    I worked on my old enough project

---
+++ conditionals/old_enough.c
@@ -0,0 +1,13 @@
+// Cairo Taylor, Old Enough Project
+#include <stdio.h>
+
+int age[20];
+
+int main(void){
+printf("How old are you chumster?");
+scanf("%s", age);
+
+if(num < 6){}
+
+    return 0;
+}
