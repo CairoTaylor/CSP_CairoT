0242dc9a91d18faf3646b484a196afa3b96efac0
Cairo Taylor <cairo.taylor@ucas-edu.net>
2025-04-29 09:37:18 -0600

    I worked on my website

--- final/final.js
+++ final/final.js
@@ -1,3 +1,5 @@
+let images = ["https://content.paulreiffer.com/wp-content/uploads/2013/08/Fine-Art-Limited-Edition-Print-Wall-Corporate-Decoration-Interior-Design-Paul-Reiffer-Photographer-Photography-High-End-Landscape-Cityscape-Buy-Rock-Of-Ages-Bryce-Canyon.jpg", "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_675,q_75,w_1200/v1/clients/swat/TitleSlide_03eb7c9a-bac5-4fbe-a2a5-b85bb82a6349.jpg"]
+
 function more(){
     document.getElementById("firstButtons").style.backgroundColor="#4189C0"
     document.getElementById("firstButtons").style.color="#FFFFFF"
@@ -21,4 +23,12 @@ function normal(){
     document.getElementById("firstButtons").style.backgroundColor="#FFD1AA"
     document.getElementById("firstButtons").style.color="#AA5939"
     document.getElementById("firstButtons").style.border="2px solid #611C00"
+}
+
+function swpi(){
+    document.getElementById("topImage").src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_675,q_75,w_1200/v1/clients/swat/TitleSlide_03eb7c9a-bac5-4fbe-a2a5-b85bb82a6349.jpg"
+}
+
+function swip(){
+    document.getElementById("topImage").src="https://content.paulreiffer.com/wp-content/uploads/2013/08/Fine-Art-Limited-Edition-Print-Wall-Corporate-Decoration-Interior-Design-Paul-Reiffer-Photographer-Photography-High-End-Landscape-Cityscape-Buy-Rock-Of-Ages-Bryce-Canyon.jpg"
 }
