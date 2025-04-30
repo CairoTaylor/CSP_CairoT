f38c8817ff01b3ff0386c362dbd21fca3e5263d7
Cairo Taylor <cairo.taylor@ucas-edu.net>
2025-04-30 09:35:19 -0600

    I probably finished my website

--- final/final.js
+++ final/final.js
@@ -6,7 +6,7 @@ function more(){
     document.getElementById("firstButtons").style.border="2px solid #004326"
   if(document.getElementById("paraTwo").style.display != "flex"){
       document.getElementById("paraTwo").style.display = "flex"
-      document.getElementById("firstButton").innerHTML = "Less About Bryce Canyon"
+      document.getElementById("firstButtons").innerHTML = "Less About Bryce Canyon"
   }else{
       document.getElementById("paraTwo").style.display = "none"
       document.getElementById("firstButton").innerHTML = "More About Bryce Canyon"
@@ -31,4 +31,11 @@ function swpi(){
 
 function swip(){
     document.getElementById("topImage").src="https://content.paulreiffer.com/wp-content/uploads/2013/08/Fine-Art-Limited-Edition-Print-Wall-Corporate-Decoration-Interior-Design-Paul-Reiffer-Photographer-Photography-High-End-Landscape-Cityscape-Buy-Rock-Of-Ages-Bryce-Canyon.jpg"
+}
+
+function broken(){
+    document.getElementById("bigTitle").innerHTML = "eao yBcnynCr"
+    document.getElementById("first").innerHTML = "fancNr iw.ao tso a sisUedoy e soravhtaaeiorcd c tyenonkntota C ohi f.rlasn ofnfaytaeeahdmeyohii dn mhTaorhtgsro mttdfof al koe mem moprnr alkPuraairisBltarnlio. nhcon r kntedgI mhboy o"
+    document.getElementById("second").innerHTML = "hn ecyuepc liatrsasTece sryokorohari hudfos e Tg e eintaxiigenpvkel r h tsaei nenpoTvrotosaa Tucdtam t dapogrsoahrsgny h eea"
+    document.getElementById("extra").innerHTML = "p ernot cv chno u. cneetnmdems toBr s e iissyu eoyge tihcue s ietsnae aleLeyyoyw ra now ieoayd0 o o bhhl C ok nfmenaershahcalgvTln.li oo,ll hl,nioeis on n8lrghgsrrcew0 bf i oa slemant lu yamu manaW d n nifa doaoe5rdco nahydgrreln rgeyoeh i B tl hv ekknunbEi e rrfehre1oDeee nosnsbBbtu2oo.mstieattoh'csebu,f nk upsnuyaeoafmr aonae skbo cnmnolirbt de au.ymtr wrco nFpwc,ny.tB isT,yan creytCi aCcarlnch c1aaloohrreiyaiyoradnhrfzhzrosla e9o ea lHl nndtlrabt u2rps Aia roo t ro m , ieaSe efs khm rew,ami o ti rnrfsr n Tun edu.erimIr aeylhi"
 }
